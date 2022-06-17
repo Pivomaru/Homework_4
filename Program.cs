430fa5370f1bb27461025519e5fe8748227a6da7
Pivomaru <spardasenpai@gmail.com>
2022-06-17 22:31:17 +0300

    DZ!

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран. */
+
+/* void FillAray(int[] fill)
+{
+    int length = fill.Length;
+    int index = 0;
+    while (index < length)
+    {
+        fill[index] = new Random().Next(0, 101);
+        index++;
+    }
+}
+
+void PrintAray(int[] print)
+{
+    int count = print.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(" " + print[pos]);
+        pos++;
+    }
+}
+
+int[] arr = new int[8];
+
+ FillAray(arr);
+ PrintAray(arr); */
+
+ /////////////////////////////////////////////
+ 
