6a74e135c524eefcae2f4997a59b701534bb8d43
Pivomaru <spardasenpai@gmail.com>
2022-06-18 00:18:16 +0300

    DZ

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ int[] arr = new int[8];
  Fill(arr);
  Print(arr); */
 
- /////////////////////////////////////////////
+/////////////////////////////////////////////
 
 
 /* Напишите программу, которая принимает на
@@ -36,13 +36,35 @@ int[] arr = new int[8];
 452 -> 11
 82 -> 10
 9012 -> 12 */
-int A = 45;
+
+/*int A = 359;
+
 int num = A % 10;
 int num1 = A / 10 % 10;
 int num2 = A /100;
 int sum = num + num1 + num2;
 
-Console.WriteLine(num);
-Console.WriteLine(num1);
-Console.WriteLine(num2);
-Console.WriteLine(sum);
+Console.WriteLine(sum); */
+
+////////////////////////////////////////////////////
+
+/*Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+int A = 2;
+int B = 4;
+int count = 0;
+while (count <= B)
+{
+int C = A * A; 
+int A1=A;
+int D = A1 * C ;;
+
+  Console.WriteLine(D);
+    count++;
+    
+}
+
